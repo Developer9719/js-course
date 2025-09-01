f7c2ffe415305e041c6323c32c2217f04f8afb25
Developer9719 <justin.bat19@gmail.com>
2025-09-01 17:28:57 -0400

    Objects, Constructors and Prototypes Lesson done

--- script.js
+++ script.js
@@ -21,40 +21,47 @@ const myObject = {
 
 // DOT notation
 myObject.property; // Returns 'value'
+// Best for retrieving properties of an object
+
 // Bracket notation
 myObject["obnoxious property"]; // Returns the function
+// Best for retrieving variables from objects and properties that have spaces in
+// the names
 
 /**
- * Object Constructors
- * - regular function with a name that starts with an upercase letter
+ * Object Constructors and Prototypes
  */
-function Player(name, marker) {
+/* Constructors
+- Only used to set up instance properties, these things will change from one instance
+to the next.
+- This is local information stored on each seperate space ship that is not needed
+by the mothership
+*/
+function Person(name, age) {
     this.name = name;
-    this.marker = marker;
-    this.sayName = function() {
-        console.log(this.name);
-    }
+    this.age = age;
 }
 
-// Calling the constructor
-const player = new Player('Steve', 'X');
-
-// Retrieve player name from object
-player.sayName(); // console logs 'Steve'
-
-// Prototype
-
-/* Exercise */
-const newBook = new Books('Book One', 'DJ MacHale', 200, 'read'); // Sends info to the constructor
-
-function Books(name, author, pages, haveRead) { // Object constructor
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.haveRead = haveRead;
-    this.info = function() {
-        return name + " by " + author + ", " + pages + " pages, " + haveRead;
-    }
+/*
+Prototype Role
+- Only used to set up methods(functions) and properties(variables) that are shared 
+amoung all instances of an object
+- Its a mothership that holds all variables and functions that each seperate space
+ship needs to function.
+*/
+Person.prototype.sayHi = function() {
+    console.log("Hi from prototype");
 }
 
-console.log(newBook.info()); // Gets return info from out of object
+/*
+Creating the new object
+- This is each seperate space ship, they have a link to the variables and functions
+the mothership stores that all space ships need to use.
+*/
+
+const person1 = new Person('Justin', 28);
+
+
+console.log(person1.name); // Returns 'Justin'
+console.log(person1.age); // Returns '28'
+person1.sayHi(); // Returns 'Hi from prototype'
